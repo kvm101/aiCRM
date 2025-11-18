a2672d204e238aba04391a7118e07b20f3b67cd3
Vasyl Karpliak <vasylko2005@gmail.com>
2025-11-18 15:50:15 +0200

    update 0.0.1

--- backend/aiCRM/src/main/java/vasyl/karpliak/aiCRM/GlobalExceptionHandler.java
+++ backend/aiCRM/src/main/java/vasyl/karpliak/aiCRM/GlobalExceptionHandler.java
@@ -9,10 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGenericException(Exception ex) {
-        // Log the exception for debugging purposes
         System.err.println("An unexpected error occurred: " + ex.getMessage());
 
-        // Create a custom error response
         String errorMessage = "An internal server error occurred.";
         return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
     }
